26ebcab61500254a75e4b6f97f02a45fc1ea6f6e
Kevin <kev.aubree@gmail.com>
2020-09-11 02:20:25 +0200

    creation de la page de contact

---
+++ src/Controller/AboutController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AboutController extends AbstractController
+{
+    /**
+     * @Route({
+     *  "fr": "/about",
+     *  "en": "/about"
+     * }, name="about")
+     */
+    public function about(Request $request)
+    {
+        dump($request);
+
+        return $this->render('default/about.html.twig', [
+            'controller_name' => 'AboutController',
+        ]);
+    }
+}
